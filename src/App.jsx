341cde72ae651b3292903662bffa2df883c7dfe4
diegotosato <tosato1998@gmail.com>
2025-11-19 14:03:18 +0100

    declare function to handle the submit, filter the films array and verify if it includes my research, if my research is empty return an empty array

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,47 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 
 function App() {
-
+  //API KEY to get the films
   const apiUrl = import.meta.env.VITE_MOVIE_DB_API_KEY
+  //state variable to contain the films
   const [films, setFilms] = useState([])
 
+  //AJAX CALL to get the films and set the variable state
   function getFilms() {
     axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiUrl}&query=un+weekend+da+bamboccioni`)
       .then(res => {
         setFilms(res.data.results)
       })
   }
-
-
+  //do the call immediatly at the mount of the component
   useEffect(getFilms, [])
 
 
 
+
+
+
+  //static array to update the state
+  const [staticFilms, setStaticFilms] = useState(films)
+  console.log(staticFilms);
+
+  //link the input change to its value
   const [search, setSearch] = useState('')
   console.log(search);
 
+
+  //handle the submit to filter the films by the search input
   function handleSubmit(e) {
     e.preventDefault()
-    setSearch('')
+    //link the reaserch into the searchbar to the films titles, filter the information to grab the films when the search is equal to a part of the title
+    const searched = films.filter(film => film.title.toLowerCase().includes(search.toLowerCase()))
+    //update staticFilms state with the filtered array
+    setStaticFilms(searched)
+
+    //when the search input is empty, update statifilms state tu empty
+    if (search.length === 0) {
+      setStaticFilms([])
+    }
   }
 
   return (
@@ -34,7 +53,7 @@ function App() {
       </form>
 
       {
-        films.map(film => (
+        staticFilms.map(film => (
           <div key={film.id}>
             <p>{film.title}</p>
             <p>{film.original_title}</p>
