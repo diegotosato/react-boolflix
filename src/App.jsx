96ac56c320dd315d180417a6ca853105a8de8041
diegotosato <tosato1998@gmail.com>
2025-11-19 18:18:48 +0100

    create function to print stars in place of opera vote, declare a cycle where compare the vote with the values of a static array that contain numbers from 1 to 5, return a span to print in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { useState, useEffect } from "react"
-import axios from "axios"
+import axios, { all } from "axios"
 import EnglandFlag from './assets/img/flags_icons/england_flag.png'
+
+
+//import FontAwesome and the Star Icon
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+
 
 function App() {
   //API KEY to get the films
@@ -46,7 +53,6 @@ function App() {
 
 
 
-
   //static array to update the state
   const [staticAll, setStaticAll] = useState(allResults)
 
@@ -70,6 +76,33 @@ function App() {
 
   }
 
+
+
+  //function to create the stars, pass the average lika a parameter
+  function printStars(vote) {
+
+    //empty array to store the stars
+    const stars = []
+
+    //valutation to compare
+    const valutation = [1, 2, 3, 4, 5]
+
+    //cycle to compare the valutation with the average
+    //if the valutation is minor then the average, fill the array with the starIcon
+
+    for (let i = 0; i < valutation.length; i++) {
+
+      //at cycle ZERO valutation is 1, average is keep from film average, in that case is 4 => 1 < 4 => fill the array with one icon
+      if (valutation[i] <= vote) {
+        stars.push(<FontAwesomeIcon key={i} icon={faStar} />)
+      }
+
+    }
+
+    //return a span tag with an amount of stars like the film average
+    return <span>{stars}</span>
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit}>
@@ -79,26 +112,35 @@ function App() {
 
       {
         staticAll.map(item => (
+
           <div key={item.id}>
+
+            {/* first part of the URL is the same, second part is object key poster_path */}
             <img src={`https://image.tmdb.org/t/p/w342${item.poster_path}`} alt={item.id} />
+
             <p>
               {
                 item.title || item.name
               }
             </p>
+
             <p>
               {
                 item.original_title || item.original_name
               }
             </p>
+
             <p>Language:
               {
                 item.original_language === 'en' ? <img src={EnglandFlag} alt="england_flag" width='20px' /> : item.original_language
               }
             </p>
+
             <p>
+              {/* invoke the function to print stars, pass the average like parameter */}
+              {/* avarage is round up to the nearest number (9.401 => 10), and divide by 2 to obtain votes between 1 and 5 */}
               {
-                Math.ceil(item.vote_average / 2)
+                printStars(Math.ceil(item.vote_average / 2))
               }
             </p>
           </div>
